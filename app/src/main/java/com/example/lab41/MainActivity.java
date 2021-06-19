75ba9dd7a97197ec835e37068ec28766e64d81b9
loter30 <loter30@gmail.com>
2021-06-19 15:32:27 +0200

    Dodanie ListView

--- app/src/main/java/com/example/lab41/MainActivity.java
+++ app/src/main/java/com/example/lab41/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.lab41;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<String> target;
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String [] colors = {"Red","Blue","Yellow","Green","Pink", "Purple","Black", "White"};
+        target = new ArrayList<String>();
+        target.addAll(Arrays.asList(colors));
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,this.target);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter((this.adapter));
     }
 }
